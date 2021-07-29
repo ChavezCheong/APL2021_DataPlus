17db44450456dcf2d72e92e7932ede5292c69c5a
Chavez Cheong <chavez.cheong@gmail.com>
2021-07-29 16:45:54 -0400

    Topic Modelling for MA

--- Data_Analysis/strrng.py
+++ Data_Analysis/strrng.py
@@ -5,3 +5,23 @@ with open(r'C:\Users\chave\Desktop\Duke\APL2021_DataPlus\Data_Analysis\stop_word
         boof += f"|{word[:-1]}"
 
 print(boof)
+
+def pivotIndex(nums):
+    left = [0]*len(nums)
+    right = [0]* len(nums)
+    # Calculate left sum array
+    left_sum = 0
+    for i in range(len(nums)):
+        left[i] = left_sum
+        left_sum += nums[i]
+    # Calculate right sum array
+    right_sum = 0
+    for j in range(len(nums)-1,-1,-1):
+        right[j] = right_sum
+        right_sum += nums[j]
+    for k in range(len(nums)):
+        if left[k] == right[k]:
+            return k
+    return -1
+
+print(pivotIndex([1,7,3,6,5,6]))
